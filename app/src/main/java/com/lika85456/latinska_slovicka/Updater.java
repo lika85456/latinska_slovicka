51bc4100cc9dbbb00de989d13fa9d236d2bdd6f9
lika85456 <lika85456@post.cz>
2017-11-07 22:27:07 +0100

    Create Updater.java

---
+++ app/src/main/java/com/lika85456/latinska_slovicka/Updater.java
@@ -0,0 +1,17 @@
+package com.lika85456.latinska_slovicka;
+
+interface IUpdater{
+    public void run();
+}
+
+public class Updater implements IUpdater{
+    public Updater(Context ctx)
+    
+    //This method update all resources
+    public void start()
+    
+    public void run()
+    {
+    
+    }
+}
